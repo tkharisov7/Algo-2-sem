939526e90f96e3897e35724167bdd31b88a1edf5
Timur <46903525+tkharisov7@users.noreply.github.com>
2021-03-04 23:25:34 +0300

    Commiting solution of 5-E, ver 2

--- contest-5-problem-E/main.cpp
+++ contest-5-problem-E/main.cpp
@@ -1,106 +1,123 @@
-#include <cstdio>
+#include <fstream>
 #include <iostream>
 #include <string>
 #include <vector>
 
-const long PRIME1 = 1117809793;
-
 class HashTable {
- private:
-  struct Node{
-    std::string key;
-    std::string val;
-    Node* child{nullptr};
-  };
  public:
-  HashTable(size_t capacity = 4) : capacity_(capacity), hash_table_(std::vector<Node*> (capacity, nullptr)) {}
+  enum class OperationType { Put, Get, Delete };
 
-  void Put(const std::string& x, const std::string& y) {
-    size_t hash_value = hash_function(x);
-    std::pair<Node*, Node*> temp = find(x);
-    Node* cur = temp.second;
+  HashTable(size_t capacity = 4) : capacity_(capacity), hash_table_(std::vector<Node*>(capacity, nullptr)) {}
+
+  void Put(const std::string& key, const std::string& value) {
+    size_t hash_value = HashFunction(key);
+    std::pair<Node*, Node*> temp = Find(key);
+    Node* current = temp.second;
     Node* parent = temp.first;
-    if (cur == nullptr) {
+    if (current == nullptr) {
       Node* new_node = new Node;
-      new_node->key = x;
-      new_node->val = y;
+      new_node->key = key;
+      new_node->val = value;
       if (parent == nullptr) {
         hash_table_[hash_value] = new_node;
       } else {
         parent->child = new_node;
       }
     } else {
-      cur->val = y;
+      current->val = value;
     }
   }
 
-  void Get(const std::string& x) {
-    std::pair<Node*, Node*> temp = find(x);
-    Node* cur = temp.second;
-    if (cur != nullptr) {
-      std::cout << cur->val << "\n";
+  std::string Get(const std::string& key) const {
+    std::pair<Node*, Node*> temp = Find(key);
+    Node* current = temp.second;
+    if (current != nullptr) {
+      return current->val;
     } else {
-      std::cout << "none" << "\n";
+      return "none";
     }
   }
 
-  void Delete(const std::string& x) {
-    size_t hash_value = hash_function(x);
-    std::pair<Node*, Node*> temp = find(x);
+  void Delete(const std::string& key) {
+    size_t hash_value = HashFunction(key);
+    std::pair<Node*, Node*> temp = Find(key);
     Node* parent = temp.first;
-    Node* cur = temp.second;
-    if (cur != nullptr) {
+    Node* current = temp.second;
+    if (current != nullptr) {
       if (parent != nullptr) {
-        parent->child = cur->child;
+        parent->child = current->child;
       } else {
         hash_table_[hash_value] = nullptr;
       }
     }
-    delete cur;
+    delete current;
   }
 
  private:
+  struct Node {
+    std::string key;
+    std::string val;
+    Node* child{nullptr};
+  };
   size_t capacity_{4};
   std::vector<Node*> hash_table_;
+  const long PRIME1 = 1117809793;
 
-  size_t hash_function(const std::string& arg) const {//using a polynomial one here
+  size_t HashFunction(const std::string& arg) const {//using a polynomial one here
     return static_cast<size_t>(std::hash<std::string>{}(arg) % PRIME1 % capacity_);
   }
 
-  std::pair<Node*, Node*> find(const std::string& arg) const {
-    size_t hash_value = hash_function(arg);
-    Node* cur = hash_table_[hash_value];
+  std::pair<Node*, Node*> Find(const std::string& key) const {
+    size_t hash_value = HashFunction(key);
+    Node* current = hash_table_[hash_value];
     Node* parent = nullptr;
-    while (cur != nullptr && cur->key != arg) {
-      parent = cur;
-      cur = cur->child;
+    while (current != nullptr && current->key != key) {
+      parent = current;
+      current = current->child;
     }
-    return {parent, cur};
+    return {parent, current};
   }
 
 };
 
+HashTable::OperationType InputToEnum(const std::string& s) {
+  if (s == "put") {
+    return HashTable::OperationType::Put;
+  }
+  if (s == "get") {
+    return HashTable::OperationType::Get;
+  }
+  if (s == "delete") {
+    return HashTable::OperationType::Delete;
+  }
+}
+
+void Solve(std::ifstream& in, std::ofstream& out, HashTable& hash_table) {
+  std::string command;
+  while (in >> command) {
+    std::string key;
+    std::string value;
+    in >> key;
+    switch (InputToEnum(command)) {
+      case HashTable::OperationType::Put:
+        in >> value;
+        hash_table.Put(key, value);
+        break;
+      case HashTable::OperationType::Get:
+        out << hash_table.Get(key) << "\n";
+        break;
+      case HashTable::OperationType::Delete:
+        hash_table.Delete(key);
+    }
+  }
+}
+
 int main() {
-  freopen("map.in", "r", stdin);
-  freopen("map.out", "w", stdout);
+  std::ifstream in ("map.in");
+  std::ofstream out("map.out");
   std::ios_base::sync_with_stdio(false);
   std::cin.tie(nullptr);
   std::cout.tie(nullptr);
-  HashTable a(100000);
-  std::string command;
-  while (std::cin >> command) {
-    std::string x;
-    std::string y;
-    std::cin >> x;
-    if (command == "put") {
-      std::cin >> y;
-      a.Put(x, y);
-    }
-    if (command == "get") {
-      a.Get(x);
-    }
-    if (command == "delete") {
-      a.Delete(x);
-    }
-  }
+  HashTable hash_table(100000);
+  Solve(in, out, hash_table);
 }
