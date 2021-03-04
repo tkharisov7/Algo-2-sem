517430b0b336f7b9ea6745b0af33e1b1d58cf2c7
Timur <46903525+tkharisov7@users.noreply.github.com>
2021-03-04 21:18:29 +0300

    Commiting solution of 5-A, ver 2

--- contest-5-problem-A/main.cpp
+++ contest-5-problem-A/main.cpp
@@ -2,79 +2,87 @@
 #include <vector>
 #include <string>
 
-const int PRIME = 1117809793;
-
 class HashTable {
  public:
-  HashTable(size_t capacity = 4) : capacity_(capacity), amount_ (0), hash_table_(std::vector<Node*> (capacity, nullptr)) {}
+  HashTable(size_t capacity = 4) : capacity_(capacity), amount_(0),
+                                   hash_table_(std::vector<Node*>(capacity, nullptr)) {}
 
-  void Add(const std::string& arg, int val, bool flag) {
-    size_t hash_value = hash_function(arg);
-    Node* cur = hash_table_[hash_value];
+  std::pair<int, bool> Add(const std::string& argument, int addition_value, bool not_resize) {
+    size_t hash_value = HashFunction(argument);
+    Node* current_node = hash_table_[hash_value];
     Node* parent = nullptr;
-    while (cur != nullptr && cur->value != arg) {
-      parent = cur;
-      cur = cur->son;
+    int return_value = -1;
+    bool is_new = true;
+    while (current_node != nullptr && current_node->value != argument) {
+      parent = current_node;
+      current_node = current_node->child;
     }
-    if (cur == nullptr) {
+    if (current_node == nullptr) {
       ++amount_;
       Node* new_node = new Node;
-      new_node->value = arg;
-      new_node->x = val;
+      new_node->value = argument;
+      new_node->x = addition_value;
       if (parent != nullptr) {
-        parent->son = new_node;
+        parent->child = new_node;
       } else {
         hash_table_[hash_value] = new_node;
       }
-      cur = new_node;
+      current_node = new_node;
     } else {
-      cur->x += val;
+      current_node->x += addition_value;
     }
-    if (flag) {
-      std::cout << cur->x << "\n";
+    if (not_resize) {
+      return_value = current_node->x;
+      is_new = false;
     }
-    if (static_cast<double>(amount_) / static_cast<double>(capacity_) > 0.95) {
+    if (static_cast<double>(amount_) / static_cast<double>(capacity_) > LOAD_FACTOR) {
       Resize();
     }
+    return {return_value, is_new};
   }
 
  private:
   size_t capacity_{4};
   size_t amount_{0};
-  struct Node{
-    std::string value{""};
+  struct Node {
+    std::string value;
     int x{0};
-    Node* son{nullptr};
+    Node* child{nullptr};
   };
   std::vector<Node*> hash_table_;
+  const int PRIME = 1117809793;
+  const double LOAD_FACTOR = 0.95;
 
-  size_t hash_function(const std::string& arg) {//using a polynomial one here
-    return static_cast<size_t>(std::hash<std::string>{}(arg) % PRIME % capacity_);
+  size_t HashFunction(const std::string& argument) const {//using a polynomial one here
+    return static_cast<size_t>(std::hash<std::string>{}(argument) % PRIME % capacity_);
   }
 
   void Resize() {
     std::vector<Node*> copy_hash_table = hash_table_;
     capacity_ *= 2;
-    hash_table_ = std::vector<Node*> (capacity_, nullptr);
+    hash_table_ = std::vector<Node*>(capacity_, nullptr);
     amount_ = 0;
     for (size_t i = 0; i < capacity_ / 2; ++i) {
-      Node* cur = copy_hash_table[i];
-      while (cur != nullptr) {
-          Add(cur->value, cur->x, false);
-          Node* t = cur;
-          cur = cur->son;
-          delete t;
+      Node* current_node = copy_hash_table[i];
+      while (current_node != nullptr) {
+        Add(current_node->value, current_node->x, false);
+        Node* t = current_node;
+        current_node = current_node->child;
+        delete t;
       }
     }
   }
 };
 
 int main() {
-  HashTable a(4);
+  HashTable device(4);
   std::string s;
   while (std::cin >> s) {
     int x;
     std::cin >> x;
-    a.Add(s, x, true);
+    std::pair<int, bool> output_value = device.Add(s, x, true);
+    if (!output_value.second) {
+      std::cout << output_value.first << "\n";
+    }
   }
 }
