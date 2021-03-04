914d5cd4ce28ee09d16afaf72c8298647c5e865e
Timur <46903525+tkharisov7@users.noreply.github.com>
2021-03-04 21:50:03 +0300

    Commiting problem 5-B, ver 2

--- contest-5-problem-B/main.cpp
+++ contest-5-problem-B/main.cpp
@@ -1,118 +1,75 @@
 #include <iostream>
 #include <vector>
 
-const int PRIME = 1117809793;
-
 class HashTable {
  private:
-  struct Node{
+  struct Node {
     int value{0};
     int x{0};
-    Node* son{nullptr};
+    Node* child{nullptr};
   };
  public:
-  HashTable(size_t capacity = 4) : capacity_(capacity), amount_(0), hash_table_(std::vector<Node*> (capacity, nullptr)) {}
+  HashTable(size_t capacity = 4) : capacity_(capacity), amount_(0),
+                                   hash_table_(std::vector<Node*>(capacity, nullptr)) {}
 
-  std::pair<Node*, Node*> Find(const int arg) {
-    size_t hash_value = hash_function(arg);
-    Node* cur = hash_table_[hash_value];
+  std::pair<Node*, Node*> Find(const int key) const {
+    size_t hash_value = hash_function(key);
+    Node* current_node = hash_table_[hash_value];
     Node* parent = nullptr;
-    while (cur != nullptr && cur->value != arg) {
-      parent = cur;
-      cur = cur->son;
+    while (current_node != nullptr && current_node->value != key) {
+      parent = current_node;
+      current_node = current_node->child;
     }
-    return {parent, cur};
+    return {parent, current_node};
   }
 
-  void Upd(const int arg, const int val) {
-    size_t hash_value = hash_function(arg);
-    std::pair<Node*, Node*> pos = Find(arg);
-    Node* cur = pos.second;
-    Node* parent = pos.first;
-    if (cur == nullptr) {
-      ++amount_;
-      Node* new_node = new Node;
-      new_node->value = arg;
-      new_node->x = val;
-      if (parent != nullptr) {
-        parent->son = new_node;
-      } else {
-        hash_table_[hash_value] = new_node;
-      }
-    }
-  }
-
-  void Oper(const int arg1, const int arg2) {
+  int Operation(const int arg1, const int arg2) {
     std::pair<Node*, Node*> pos1 = Find(arg1);
     std::pair<Node*, Node*> pos2 = Find(arg2);
     if (pos1.second == nullptr) {
-      ++amount_;
-      size_t hash_value = hash_function(arg1);
-      Node* new_node = new Node;
-      new_node->value = arg1;
-      new_node->x = arg1;
-      new_node->son = nullptr;
-      if (pos1.first != nullptr) {
-        pos1.first->son = new_node;
-      } else {
-        hash_table_[hash_value] = new_node;
-      }
-      pos1.second = new_node;
+      create_node(pos1, arg1);
     }
     if (pos2.second == nullptr) {
-      ++amount_;
-      size_t hash_value = hash_function(arg2);
-      Node* new_node = new Node;
-      new_node->value = arg2;
-      new_node->x = arg2;
-      new_node->son = nullptr;
-      if (pos2.first != nullptr) {
-        pos2.first->son = new_node;
-      } else {
-        hash_table_[hash_value] = new_node;
-      }
-      pos2.second = new_node;
+      create_node(pos2, arg2);
     }
     std::swap(pos1.second->x, pos2.second->x);
-    //if (static_cast<double>(amount_) / static_cast<double>(capacity_) > 0.95) {
-    //  Resize();
-    //}
-    std::cout << std::abs(pos1.second->x - pos2.second->x) << "\n";
+    return std::abs(pos1.second->x - pos2.second->x);
   }
+
  private:
   size_t capacity_{4};
   size_t amount_{0};
   std::vector<Node*> hash_table_;
+  const int PRIME = 1117809793;
 
-  size_t hash_function(const int arg) {//using a polynomial one here
-    return static_cast<size_t>(std::hash<int>{}(arg) % PRIME % capacity_);
+  size_t hash_function(const int argument) const {//using a polynomial one here
+    return static_cast<size_t>(std::hash<int>{}(argument) % PRIME % capacity_);
   }
 
-  void Resize() {
-    std::vector<Node*> copy_hash_table = hash_table_;
-    capacity_ *= 2;
-    hash_table_ = std::vector<Node*> (capacity_, nullptr);
-    amount_ = 0;
-    for (size_t i = 0; i < capacity_ / 2; ++i) {
-      Node* cur = copy_hash_table[i];
-      while (cur != nullptr) {
-        Upd(cur->value, cur->x);
-        Node* t = cur;
-        cur = cur->son;
-        delete t;
-      }
+  void create_node(std::pair<Node*, Node*>& position, const int value) {
+    ++amount_;
+    size_t hash_value = hash_function(value);
+    Node* new_node = new Node;
+    new_node->value = value;
+    new_node->x = value;
+    new_node->child = nullptr;
+    if (position.first != nullptr) {
+      position.first->child = new_node;
+    } else {
+      hash_table_[hash_value] = new_node;
     }
+    position.second = new_node;
   }
 };
 
 int main() {
-  HashTable a(200000);
-  int t;
-  std::cin >> t;
-  while (t) {
+  HashTable hash_table(200000);
+  int n;
+  std::cin >> n;
+  while (n) {
     int x, y;
     std::cin >> x >> y;
-    a.Oper(x, y);
-    --t;
+    std::cout << hash_table.Operation(x, y) << "\n";
+    --n;
   }
 }
